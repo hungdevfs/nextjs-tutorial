dc6a7da98b2b7cdf91b41bfcd6e76707aef12548
jack nguyen <jack.nguyen@helius-software.com>
2021-11-17 13:43:13 +0700

    fix style navbar footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import styles from '../styles/components/Navbar.component.module.css';
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/ninjas">Ninja list</Link>
+      <div>
+        <Image src="/images/logo.png" width={150} height={60} />
+      </div>
+      <div className={styles.nav}>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/ninjas">Ninja list</Link>
+      </div>
     </div>
   );
 };
